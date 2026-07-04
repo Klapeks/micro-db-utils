3f9fc2e0630dd72aca54ddaeda541fc3f750850a
Klapeks <sashaKaraguts@gmail.com>
2026-07-04 18:09:26 +0300

    dataSourceQuery

--- dist/micro.sql.d.ts
+++ dist/micro.sql.d.ts
@@ -1,7 +1,8 @@
 import { DatabaseOptions } from "@klapeks/utils";
-import { SQLAlterCommand, SQLSelectCommands, SQLTablesCommands, SQLTimeCommandsExpressions } from "./sql";
-import { DataSourceOptions } from "typeorm";
+import { ISQLCommandAdapter, SQLAlterCommand, SQLCommandData, SQLSelectCommands, SQLTablesCommands, SQLTimeCommandsExpressions } from "./sql";
+import { DataSource, DataSourceOptions } from "typeorm";
 export declare namespace MicroSQL {
+    function dataSourceQuery<T = any>(dataSource: DataSource, cb: ISQLCommandAdapter | SQLCommandData | string | ((dbtype: DatabaseOptions['type'], database: string) => ISQLCommandAdapter | SQLCommandData | string | Promise<ISQLCommandAdapter | SQLCommandData | string>)): Promise<T[]>;
     function alter(table: string): SQLAlterCommand;
     function select(table: string): SQLSelectCommands;
     function tables(): typeof SQLTablesCommands;
--- src/micro.sql.ts
+++ src/micro.sql.ts
@@ -1,8 +1,32 @@
 import { DatabaseOptions } from "@klapeks/utils";
-import { SQLAlterCommand, SQLSelectCommands, SQLTablesCommands, SQLTimeCommandsExpressions } from "./sql";
-import { DataSourceOptions } from "typeorm";
+import { ISQLCommandAdapter, SQLAlterCommand, SQLCommandData, SQLSelectCommands, SQLTablesCommands, SQLTimeCommandsExpressions } from "./sql";
+import { DataSource, DataSourceOptions } from "typeorm";
 
 export namespace MicroSQL {
+
+    export async function dataSourceQuery<T = any>(
+        dataSource: DataSource, 
+        cb: ISQLCommandAdapter | SQLCommandData | string | ((
+            dbtype: DatabaseOptions['type'],
+            database: string
+        ) => ISQLCommandAdapter | SQLCommandData | string 
+            | Promise<ISQLCommandAdapter | SQLCommandData | string>)
+    ): Promise<T[]> {
+        const dbType = dataSource.options.type as any as DatabaseOptions['type'];
+        let query: string | ISQLCommandAdapter | SQLCommandData = (
+            typeof cb == 'function' ? await cb(
+                dbType, dataSource.options.database as any
+            ) : cb
+        );
+        if (typeof query === 'string') return dataSource.query(query);
+        if (typeof query === 'object' && !('query' in query)) {
+            if (dbType === 'mysql') query = query.toMySQL({});
+            else if (dbType === 'mssql') query = query.toMSSQL({});
+            else throw "Unknown database type: " + dbType;
+        }
+        if (typeof query === 'string') return dataSource.query(query);
+        return dataSource.query(query.query, query.params);
+    }
 
     export function alter(table: string) {
         return new SQLAlterCommand(table);
